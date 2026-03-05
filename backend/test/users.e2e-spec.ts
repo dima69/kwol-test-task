cc08f768d84afd36374efd4209d833310dd18128
dima69 <44449344+dima69@users.noreply.github.com>
2026-03-05 12:54:58 +0300

    test(backend): add users e2e test

---
+++ backend/test/users.e2e-spec.ts
@@ -0,0 +1,41 @@
+import 'dotenv/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { PrismaService } from '../src/prisma/prisma.service';
+
+describe('Users (e2e)', () => {
+  let app: INestApplication<App>;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+
+    prisma = app.get(PrismaService);
+  });
+
+  beforeEach(async () => {
+    await prisma.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+    await app.close();
+  });
+
+  it('GET /users - should return empty array', () => {
+    // prettier-ignore
+    return request(app.getHttpServer())
+      .get('/users')
+      .expect(200)
+      .expect([]);
+  });
+});
